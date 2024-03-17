1d955f1ca57f4e6ca6dc1409c317e77a73b726d6
Jordan Harband <ljharb@gmail.com>
2024-03-17 00:02:31 -0700

    [Refactor] replace `es-abstract` with `es-object-atoms` when possible; update `define-properties`, `es-abstract`

--- implementation.js
+++ implementation.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var RequireObjectCoercible = require('es-abstract/2023/RequireObjectCoercible');
-var ObjectDefineProperties = require('es-abstract/2023/ObjectDefineProperties');
+var RequireObjectCoercible = require('es-object-atoms/RequireObjectCoercible');
+var ObjectDefineProperties = require('es-abstract/2024/ObjectDefineProperties');
 
 module.exports = function defineProperties(O, P) {
 	RequireObjectCoercible(O);
